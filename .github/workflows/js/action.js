b7cd9308f9a68bd540e63f7ca3ace67bbe34f07a
Ilya Melnikov <i486dx@yandex.ru>
2022-08-03 17:48:33 +0400

    Workflow update

---
+++ .github/workflows/js/action.js
@@ -0,0 +1,28 @@
+const _ = require('lodash')
+const Jira = require('./common/net/Jira')
+
+module.exports = class {
+  constructor ({ githubEvent, argv, config }) {
+    this.Jira = new Jira({
+      baseUrl: config.baseUrl,
+      token: config.token,
+      email: config.email,
+    })
+
+    this.config = config
+    this.argv = argv
+    this.githubEvent = githubEvent
+  }
+
+  async execute () {
+    const { argv } = this
+
+    const issueId = argv.issue
+    const issue = await this.Jira.getIssue(issueId)
+    const status = _.get(issue, 'fields.status.name')
+
+    console.log(`${issueId} status is : ${status} .`)
+
+    return { status: status }
+  }
+}
---
+++ .github/workflows/js/index.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const YAML = require('yaml')
+const core = require('@actions/core')
+
+const configPath = `${process.env.HOME}/jira/config.yml`
+const Action = require('./action')
+
+// eslint-disable-next-line import/no-dynamic-require
+const githubEvent = require(process.env.GITHUB_EVENT_PATH)
+const config = YAML.parse(fs.readFileSync(configPath, 'utf8'))
+
+async function exec () {
+  try {
+    const result = await new Action({
+      githubEvent,
+      argv: parseArgs(),
+      config,
+    }).execute()
+
+    if (result) {
+      const extendedConfig = Object.assign({}, config, result)
+
+      fs.writeFileSync(configPath, YAML.stringify(extendedConfig))
+
+      return
+    }
+
+    console.log('Failed to find issue.')
+    process.exit(78)
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+function parseArgs () {
+
+  return {
+    issue: core.getInput('issue')
+  }
+}
+
+exec()
